cf8ea6158eb306317bc5af1fab7894c9d719efef
Yang Xing <XingYang0827>
2025-10-12 21:31:14 -0400

    fix no reading

--- STM32F4_I2C/app/main.c
+++ STM32F4_I2C/app/main.c
@@ -1,143 +1,3 @@
-//#include <stdint.h>
-//#include <stdbool.h>
-//#include <string.h>
-//#include "stm32f4xx.h"
-//#include "stm32f4xx_i2c.h"
-//#include "cpu_delay.h"
-//#include "usart.h"
-
-//#define EEPROM_ADDR  0x50  // A0=A1=A2=0
-//#define EEPROM_STM32_ADDR 0xA0
-
-//void I2C1_Init(void) {
-//    GPIO_InitTypeDef GPIO_InitStruct;
-//    I2C_InitTypeDef  I2C_InitStruct;
-
-//    // PB6 = SCL, PB9 = SDA
-//    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_I2C1);
-//    GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
-
-//    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_9;
-//    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-//    GPIO_InitStruct.GPIO_Speed = GPIO_High_Speed;
-//    GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;  
-//    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP; 
-//    GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-//    I2C_DeInit(I2C1);
-//    I2C_InitStruct.I2C_ClockSpeed = 100ul * 1000ul;
-//    I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
-//    I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
-//    I2C_InitStruct.I2C_OwnAddress1 = 0x00;
-//    I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
-//    I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-//    I2C_Init(I2C1, &I2C_InitStruct);
-
-//    I2C_Cmd(I2C1, ENABLE);
-//}
-
-//#define I2C_CHECK_EVENT(EVENT, TIMEOUT) \
-//	do { \
-//		int timeout = TIMEOUT; \
-//		while (!I2C_CheckEvent(I2C1, EVENT) && timeout > 0) { \
-//				cpu_delay(10); \
-//				timeout -= 10; \
-//			} \
-//		if (timeout <= 0) {return false;} \
-//	} while (0)
-//	
-//	
-//static bool EEPROM_Page_Write(uint16_t MemAddress, uint8_t data[], uint32_t length) {
-//    // wait until line is empty
-//		while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY));
-//	
-//    I2C_GenerateSTART(I2C1, ENABLE);
-//    I2C_CHECK_EVENT(I2C_EVENT_MASTER_MODE_SELECT, 1000);
-//	
-//    I2C_Send7bitAddress(I2C1, EEPROM_ADDR << 1, I2C_Direction_Transmitter);
-//    I2C_CHECK_EVENT(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED, 1000);
-
-//    // send high address bytes
-//    I2C_SendData(I2C1, (uint8_t)(MemAddress >> 8));
-//    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
-
-//    // send low address bytes
-//    I2C_SendData(I2C1, (uint8_t)(MemAddress & 0xFF));
-//    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
-
-//    // send data
-//		for (uint32_t i = 0; i < length; i++) {
-//			I2C_SendData(I2C1, data[i]);
-//			I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
-//		}
-
-//    I2C_GenerateSTOP(I2C1, ENABLE);
-//		
-//		return true;
-//}
-
-//	
-//static bool EEPROM_Page_Read(uint16_t MemAddress, uint8_t data[], uint32_t length) {
-//		while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY));
-
-//    I2C_GenerateSTART(I2C1, ENABLE);
-//		I2C_AcknowledgeConfig(I2C1, ENABLE);
-//		I2C_CHECK_EVENT(I2C_EVENT_MASTER_MODE_SELECT, 1000);
-//   
-//    I2C_Send7bitAddress(I2C1, EEPROM_ADDR << 1, I2C_Direction_Transmitter);
-//    I2C_CHECK_EVENT(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED, 1000);
-
-//    // send register address
-//    I2C_SendData(I2C1, (uint8_t)(MemAddress >> 8));
-//    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
-
-//    I2C_SendData(I2C1, (uint8_t)(MemAddress & 0xFF));
-//    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
-
-//		I2C_AcknowledgeConfig(I2C1, ENABLE);
-//    // restart
-//    I2C_GenerateSTART(I2C1, ENABLE);
-//    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-
-//    I2C_Send7bitAddress(I2C1, EEPROM_ADDR << 1, I2C_Direction_Receiver);
-//    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
-
-//		// send data
-//		for (uint32_t i = 0; i < length; i++) {
-//			if (i == length - 1) {
-//					I2C_AcknowledgeConfig(I2C1, DISABLE);
-//					I2C_GenerateSTOP(I2C1, ENABLE);
-//			}
-//			while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
-//			data[i] = I2C_ReceiveData(I2C1);
-//		}
-//		I2C_AcknowledgeConfig(I2C1, ENABLE);
-//		
-//		return true;
-//}
-
-//static uint8_t testnums[200];
-
-//int main (void) {
-//	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-//  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
-//	
-//	I2C1_Init();
-//	usart_init();
-//	
-//	for (uint32_t i= 0; i < 200; i++) {
-//		testnums[i] = i+1;
-//	}
-//	
-//	EEPROM_Page_Write(0x0000, testnums, 200);
-//	cpu_delay(5 * 1000);
-//	memset(testnums, 0, 200);
-//	EEPROM_Page_Read(0x0000, testnums, 200);
-//	while(1);
-//}
-
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
@@ -147,8 +7,8 @@
 #include "usart.h"
 
 
-#define EEPROM_ADDR  0x50  // A0=A1=A2=0
-#define EEPROM_STM32_ADDR 0xA0
+#define EEPROM_ADDR  0x50  // A0=A1=A2=0 0b1010000 (7 bit)
+#define EEPROM_STM32_ADDR 0xA0 // 0b10100000 (8 bit)
 
 void I2C1_Init(void) {
     GPIO_InitTypeDef GPIO_InitStruct;
@@ -175,6 +35,7 @@ void I2C1_Init(void) {
     I2C_Init(I2C1, &I2C_InitStruct);
 
     I2C_Cmd(I2C1, ENABLE);
+		cpu_delay(100);
 }
 
 #define I2C_CHECK_EVENT(EVENT, TIMEOUT) \
@@ -182,7 +43,7 @@ void I2C1_Init(void) {
         uint32_t timeout = TIMEOUT; \
         while (!I2C_CheckEvent(I2C1, EVENT) && timeout > 0) { \
             cpu_delay(10); \
-            timeout -= 10; \
+            timeout -= 1; \
         } \
         if (timeout <= 0) \
             return false; \
@@ -191,51 +52,64 @@ void I2C1_Init(void) {
 
 static bool eeprom_page_write(uint16_t address, uint8_t data[], uint32_t length)
 {
+		while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY));
     I2C_AcknowledgeConfig(I2C1, ENABLE);
     I2C_GenerateSTART(I2C1, ENABLE);
-    I2C_CHECK_EVENT(I2C_EVENT_MASTER_MODE_SELECT, 1000);
-    I2C_Send7bitAddress(I2C1, EEPROM_ADDR<<1, I2C_Direction_Transmitter);
+    I2C_CHECK_EVENT(I2C_EVENT_MASTER_MODE_SELECT, 10000);
+    I2C_Send7bitAddress(I2C1, EEPROM_STM32_ADDR, I2C_Direction_Transmitter);
+		//cpu_delay(100);
     I2C_CHECK_EVENT(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED, 1000);
     I2C_SendData(I2C1, (address >> 8) & 0xff);
-    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTING, 1000);
+    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
     I2C_SendData(I2C1, address & 0xff);
-    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTING, 1000);
+    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
     for (uint32_t i = 0; i < length; i++)
     {
         I2C_SendData(I2C1, data[i]);
-        I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTING, 1000);
+        I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
     }
+//		while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+//		(void)I2C1->SR1;     // ? SR1 ???
+//		(void)I2C1->SR2;     // ? SR2 ???
     I2C_GenerateSTOP(I2C1, ENABLE);
-    
+//		while (I2C_GetFlagStatus(I2C1, I2C_FLAG_STOPF) == RESET);
+//		I2C_ClearFlag(I2C1, I2C_FLAG_STOPF);
+    // cpu_delay(10*1000);
     return true;
 }
-
 static bool eeprom_page_read(uint16_t address, uint8_t data[], uint32_t length)
 {
+		// while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY));
     I2C_AcknowledgeConfig(I2C1, ENABLE);
     I2C_GenerateSTART(I2C1, ENABLE);
     I2C_CHECK_EVENT(I2C_EVENT_MASTER_MODE_SELECT, 1000);
-    I2C_Send7bitAddress(I2C1, EEPROM_ADDR<<1, I2C_Direction_Transmitter);
+
+    I2C_Send7bitAddress(I2C1, EEPROM_STM32_ADDR, I2C_Direction_Transmitter);
     I2C_CHECK_EVENT(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED, 1000);
+
     I2C_SendData(I2C1, (address >> 8) & 0xff);
-    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTING, 1000);
+    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
     I2C_SendData(I2C1, address & 0xff);
-    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTING, 1000);
-    
-    I2C_AcknowledgeConfig(I2C1, ENABLE);
+    I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_TRANSMITTED, 1000);
+
+    cpu_delay(10000); // give eeprom time to prep for repeated start
+		I2C_AcknowledgeConfig(I2C1, ENABLE);
     I2C_GenerateSTART(I2C1, ENABLE);
     I2C_CHECK_EVENT(I2C_EVENT_MASTER_MODE_SELECT, 1000);
-    I2C_Send7bitAddress(I2C1, EEPROM_ADDR<<1, I2C_Direction_Receiver);
+
+    I2C_Send7bitAddress(I2C1, EEPROM_STM32_ADDR, I2C_Direction_Receiver);
     I2C_CHECK_EVENT(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED, 1000);
-    for (uint32_t i = 0; i < length; i++)
+
+    for (int i = 0; i < length; i++)
     {
         if (i == length - 1)
             I2C_AcknowledgeConfig(I2C1, DISABLE);
         I2C_CHECK_EVENT(I2C_EVENT_MASTER_BYTE_RECEIVED, 1000);
         data[i] = I2C_ReceiveData(I2C1);
     }
+
     I2C_GenerateSTOP(I2C1, ENABLE);
-    
+    I2C_AcknowledgeConfig(I2C1, ENABLE); // re-enable for next op
     return true;
 }
 
@@ -243,17 +117,25 @@ static bool eeprom_page_read(uint16_t address, uint8_t data[], uint32_t length)
 static uint8_t testnums[16];
 
 int main (void) {
+	cpu_delay(10000); 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
 	
 	I2C1_Init();
+	cpu_delay(10000);  
 	
 	for (uint32_t i= 0; i < 16; i++) {
 		testnums[i] = i+1;
 	}
 	
-	eeprom_page_write(0x0000, testnums, 16);
+	bool b1 = eeprom_page_write(0x0000, testnums, 16);
 	memset(testnums, 0, 16);
-	eeprom_page_read(0x0000, testnums, 16);
+
+	/* reset + reinitialization */
+	I2C_Cmd(I2C1, DISABLE);
+	I2C_DeInit(I2C1);
+	I2C1_Init();
+	
+	bool b2 = eeprom_page_read(0x0000, testnums, 16);
 	while(1);
 }
--- STM32F4_I2C/driver/cpu_delay/cpu_delay.c
+++ STM32F4_I2C/driver/cpu_delay/cpu_delay.c
@@ -10,7 +10,7 @@ void cpu_delay(uint32_t us)
         SysTick->VAL = 0;
         SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
         while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
-        SysTick->CTRL = ~SysTick_CTRL_ENABLE_Msk;
+        SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
         us -= 1000;
     }
     
@@ -20,6 +20,6 @@ void cpu_delay(uint32_t us)
         SysTick->VAL = 0;
         SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
         while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
-        SysTick->CTRL = ~SysTick_CTRL_ENABLE_Msk;
+        SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
     }
 }
